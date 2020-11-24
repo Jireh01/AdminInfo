023c163321798379cce982f26bcff475df4a5755
Francesco <francsco8bassino@gmail.com>
2020-11-24 12:32:59 -0500

    commit

--- Codigo/Exploracion-Frany.R
+++ Codigo/Exploracion-Frany.R
@@ -69,6 +69,11 @@ head(q11)
 q12 <- licenciamiento %>% select(CODIGO_ENTIDAD, NOMBRE, DEPARTAMENTO_LOCAL) %>% filter(licenciamiento$ESTADO_LICENCIAMIENTO == 'LICENCIA OTORGADA', licenciamiento$PERIODO_LICENCIAMIENTO == 6, licenciamiento$DEPARTAMENTO_LOCAL == 'LIMA') %>% group_by(NOMBRE)
 head(q12)
 
+q13 <- quantile(carnesEx$Cant_Carnes, probs = seq(0, 1, 0.25), na.rm = TRUE, names = TRUE, type = 7)
+q13
+
+q14 <-  quantile(licenciamiento$PERIODO_LICENCIAMIENTO, probs = seq(0, 1, 0.25), na.rm = TRUE, names = TRUE, type = 7)
+q14
 
 
 ######### Graficos ##########
