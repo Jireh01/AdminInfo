7332003d4a4a4af4416319b5a4eb718cc174eb9b
Francesco <francsco8bassino@gmail.com>
2020-11-28 01:01:17 -0500

    commit

--- Proyecto/Codigo/6 - Shiny/www/correo.R
+++ Proyecto/Codigo/6 - Shiny/www/correo.R
@@ -1,17 +1,12 @@
 library(rJava)
-library("mailR")
+library(mailR)
 
-send.mail(from = "----------------",
-          to = c("correo1@gmail.com", "francesco8bassino@gmail.com"), 
+send.mail(from = "francesco_bassino@hotmail.com",
+          to = c("renxzen@gmail.com", "u201816649@upc.edu.pe", "francesco8bassino@gmail.com"), 
           subject = "Trabajo Final de Administracion de la Informacion!!!",
           body = "Hola, este es un mensaje para mi compañero de grupo de Adminfo!!!",
           smtp = list(host.name = "smtp.gmail.com", port = 465,
-                      user.name = "josezea@usantotomas.edu.co",
-                      passwd = "contraseña", ssl = TRUE),  
-          authenticate = TRUE,
-          send = TRUE,
-          attach.files = c("wordcloud.png" ),
-          file.names = c( "de palabras.png"), # El nombre con el que se quiere adjuntar el correo
-          file.descriptions = c("Archivo que contiene nube de palabras"),
-          # optional parameter
-          debug = TRUE)
+                      user.name = "elsolido15@gmail.com",
+                      passwd = "amoelsushi88", ssl = TRUE),  
+          authenticate = TRUE)
+getwd()
